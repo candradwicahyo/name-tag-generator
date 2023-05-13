7175cddebf0db75a6fefa1cf72d70cd0459c7972
candradwicahyo <candradwicahyo18@gmail.com>
2023-05-13 09:24:59 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Name Tag Generator
+
+Program "Name Tag Generator" adalah sebuah aplikasi web yang dibuat dengan menggunakan bahasa HTML, CSS, dan JavaScript. Tujuan dari aplikasi ini adalah untuk menghasilkan nama pengguna yang dimasukkan oleh pengguna, yang kemudian akan tampil di halaman depan. 
+
+Salah satu fitur yang menonjol dari aplikasi ini adalah penggunaan library SweetAlert2, yang memungkinkan pesan untuk ditampilkan secara elegan ketika data baru berhasil ditambahkan atau data berhasil dihapus. 
+
+Selain itu, program ini juga memiliki fitur yang cerdas, di mana jika pengguna memasukkan nama yang sama dengan nama pengguna lain, maka program akan memberikan pesan "data is already exist" yang menandakan bahwa nama tersebut sudah dipakai oleh orang lain. 
+
+Untuk memudahkan penggunaan, aplikasi ini menggunakan localstorage untuk menyimpan data sementara waktu, sehingga pengguna dapat dengan mudah mengakses data yang telah dimasukkan sebelumnya. Dengan demikian, aplikasi "Name Tag Generator" menjadi sebuah solusi yang tepat bagi mereka yang membutuhkan aplikasi untuk membuat dan menampilkan nama pengguna dengan cepat dan mudah.
+
+## Tools dan Bahasa yang Digunakan
+
+* html
+* css
+* javascript
+
+## Lainnya
+
+* [SweetAlert2](https://sweetalert2.github.io)
+
+## kontributor
+
+* candra dwi cahyo
+
+  * [facebook](https://facebook.com/candradwicahyo18)
+  * [instagram](https://instagram.com/candradwicahyo18)
+
+## Cara Mengunduh
+
+Untuk mengunduh, buka command prompt pada perangkat Anda, lalu install Git dan ketikkan kode berikut:
+
+```bash 
+git clone https://github.com/candradwicahyo/name-tag-generator.git
+```
+
+## Preview dan Demo
+
+Berikut adalah tampilan preview dari repository:
+
+![preview](https://github.com/candradwicahyo/name-tag-generator/blob/master/image.jpg)
+
+Untuk mencoba demo, silakan kunjungi [tautan ini](https://candradwicahyo.github.io/name-tag-generator)
---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+window.addEventListener('DOMContentLoaded', () => {
+  
+  let items = [];
+  
+  const content = document.querySelector('.content');
+  const input = document.querySelector('.input');
+  const buttonSubmit = document.querySelector('.button-submit');
+  buttonSubmit.addEventListener('click', addItem);
+  
+  function addItem(event) {
+    event.preventDefault();
+    const value = input.value.trim();
+    const data = {value: value};
+    if (validate(value)) {
+      if (isItemExist(value)) return alerts('error', 'Name Tag is already exist!');
+      items.unshift(data);
+      setLocalstorage('item', items);
+      showElement(data);
+      alerts('success', 'Item has been added!');
+      showData();
+      clear();
+    }
+  }
+  
+  function validate(value) {
+    if (!value) return alerts('error', 'Input is empty!');
+    if (value.length > 20) return alerts('error', 'Your name must be no more than 20 character');
+    if (value.match(/[0-9]/gi)) return alerts('error', 'Name should be letters!');
+    return true;
+  }
+  
+  function isItemExist(value) {
+    let exist = false;
+    items.forEach(item => {
+      if (item.value.toLowerCase() === value.toLowerCase()) exist = true;
+    });
+    return exist;
+  }
+  
+  function setLocalstorage(name, value) {
+    localStorage.setItem(name, JSON.stringify(value));
+  }
+  
+  function showElement(data, index = 0) {
+    const result = renderElement(data, index);
+    content.insertAdjacentHTML('beforeend', result);
+  }
+  
+  function showData() {
+    content.innerHTML = '';
+    const data = localStorage.getItem('item');
+    items = (data) ? JSON.parse(data) : [];
+    items.forEach((item, index) => showElement(item, index));
+  }
+  
+  showData();
+  
+  function renderElement(data, index) {
+    return `
+      <div class="content-wrapper">
+        <div class="box">
+          <span class="close-button button-delete" data-index="${index}">×</span>
+          <h3>Hi, my name is</h3>
+          <h6>${data.value}</h6>
+        </div>
+      </div>
+    `;
+  }
+  
+  function alerts(icon, text) {
+    swal.fire ({
+      icon: icon,
+      title: 'alert',
+      text: text
+    });
+  }
+  
+  function clear() {
+    const forms = document.querySelectorAll('.form');
+    forms.forEach(form => form.reset());
+  }
+  
+  window.addEventListener('click', event => {
+    if (event.target.classList.contains('button-delete')) {
+      const index = event.target.dataset.index;
+      deleteItem(index);
+    }
+  });
+  
+  function deleteItem(index) {
+    swal.fire ({
+      icon: 'info',
+      title: 'are you sure?',
+      text: 'do you want to delete this item?',
+      showCancelButton: true
+    })
+    .then(response => {
+      if (response.isConfirmed) {
+        items.splice(index, 1);
+        setLocalstorage('item', items);
+        alerts('success', 'Item has been deleted!');
+        showData();
+      }
+    });
+  }
+  
+});
